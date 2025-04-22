bbb941c867637da51a9b47d3588a1b48397b1234
nahashon <nashonmwendwa0@gmail.com>
2025-04-23 01:55:44 +0300

    made correction on the indentation

--- app/schemas.py
+++ app/schemas.py
@@ -3,20 +3,16 @@ from typing import Optional
 from datetime import datetime
 
 
-
 #--------------------------------Product Schemas-------------------------------
-# Shared product fields
 class ProductBase(BaseModel):
     name: str = Field(..., min_length=2, max_length=100)
     description: Optional[str] = Field(None, max_length=300)
     price: float = Field(..., gt=0, description="Must be a positive number")
     quantity: int = Field(..., ge=1, description="Minimum quantity is 1")
 
-# Product creation request
 class ProductCreate(ProductBase):
     pass
 
-# Product response
 class ProductResponse(ProductBase):
     id: int
     status: str
@@ -26,13 +22,10 @@ class ProductResponse(ProductBase):
 
 
 #-------------------------------Order Schemas---------------------------------------
-
-# Order creation request
 class OrderCreate(BaseModel):
     product_id: int = Field(..., gt=0)
     quantity: int = Field(..., ge=1, description="Minimum order quantity is 1")
 
-# Order response
 class OrderResponse(BaseModel):
     id: int
     product_id: int
@@ -42,21 +35,19 @@ class OrderResponse(BaseModel):
 
     class Config:
         orm_mode = True
-        
-        
+
 
 #-------------------------------------Payment Schemas--------------------------
-# Payment
 class PaymentRequest(BaseModel):
     order_id: int = Field(..., gt=0)
     amount: float = Field(..., gt=0)
     payment_method: str = Field(..., min_length=3)
-    
+
     @validator("payment_method")
     def validate_method(cls, v):
-    if v.lower() not in ["paypal", "stripe","mpesa"]:
-        raise ValueError("Invalid payment method. Must be 'paypal', 'stripe', or 'mpesa'")
-    return v.lower()
+        if v.lower() not in ["paypal", "stripe", "mpesa"]:
+            raise ValueError("Invalid payment method. Must be 'paypal', 'stripe', or 'mpesa'")
+        return v.lower()
 
 
 class PaymentResponse(BaseModel):
