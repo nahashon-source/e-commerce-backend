d27622e4b8253906091a659c416f804ef75c808f
nahashon <nashonmwendwa0@gmail.com>
2025-04-28 01:02:26 +0300

    removed the databaseURL

--- app/database.py
+++ app/database.py
@@ -6,10 +6,9 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-DATABASE_URL = os.getenv("DATABASE_URL")      
 
 # Creates the DB Connection
-engine = create_engine(DATABASE_URL) 
+engine = create_engine("DATABASE_URL") 
 
 #allow us to interact with the DB (opening a sesssion)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
