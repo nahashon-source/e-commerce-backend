dd4c6efc1ba4eccd8739df042ed6bb74aff872e5
nahashon <nashonmwendwa0@gmail.com>
2025-04-28 13:47:01 +0300

    update the file to import engine and on config addded a comment

--- app/config.py
+++ app/config.py
@@ -1,6 +1,7 @@
 from dotenv import load_dotenv
 import os
 
+#Load environment variables from .env
 load_dotenv()
 
 
--- app/test_database.py
+++ app/test_database.py
@@ -1,2 +1,5 @@
 #📘 Why: Teaches best practices + helps you test without ruining real data
 
+from app.database import engine
+
+print(engine)
