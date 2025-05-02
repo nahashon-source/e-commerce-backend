72cb6cee6694d4d480db4e895792215f6b4c5bdd
nahashon <nashonmwendwa0@gmail.com>
2025-05-02 13:34:08 +0300

    fix(database): correctly use DATABASE_URL from .env when creating engine, - Fixed issue where the engine was initialized with a string literal instead of the environment value, - Reordered lines for clean configuration and best practices.

--- app/database.py
+++ app/database.py
@@ -1,26 +1,24 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
-import os #module for handling environments configurations
+import os  # module for handling environment configurations
 from dotenv import load_dotenv
 
 
-#dotenv best practices
-load_dotenv() #load variables from .env
+# Load variables from .env
+load_dotenv()
 
 
-# Creates the DB Connection
-engine = create_engine("DATABASE_URL") 
-
-
-#safely fetches the value from .env
+# Safely fetch the value from .env
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-#allow us to interact with the DB (opening a sesssion)
+# Create the DB Connection
+engine = create_engine(DATABASE_URL)
+
+
+# Allow us to interact with the DB (opening a session)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-#define models(tables)
+# Define models (tables)
 Base = declarative_base()
 
-
-
-#THIS FILE READS FROM THE .env
+# THIS FILE READS FROM THE .env
